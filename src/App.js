a0793d353d83ea50afbf886d5a3dfcd4322e9609
Moabe <moabeluis2018@gmail.com>
2022-03-31 20:52:01 -0300

    Teste Finalizado

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-        <h1>Hello world</h1>
-    </div>
-  );
+import { Title, TitleSmall } from "./styles"
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <Title fontSize={20}>
+            Hello world
+            <span>texto menor</span>
+          </Title>
+
+          <TitleSmall>Menor</TitleSmall>
+      </div>
+    );
+  }
 }
 
 export default App;
